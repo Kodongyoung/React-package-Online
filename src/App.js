99a45abaeb3cc82780ec2dca9d97796fe16b2c56
Kodongyoung <ehddud3254@naver.com>
2021-11-22 17:29:01 +0900

    frist commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Composition from "./components/2-4.Props/Composition";
+import Extarction from "./components/2-4.Props/Extraction/Extarction";
+
+function App() {
+  return (
+    <div className="App">
+      <Extarction />
+      <Composition />
+    </div>
+  );
+}
+
+export default App;
